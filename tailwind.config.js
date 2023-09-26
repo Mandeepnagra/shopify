56b02ec3b98ac6e60a8fbb0816b09e8df237bc25
Mandeep Nagra <mandeepnagra707@gmail.com>
2023-09-26 15:59:34 -0400

    timeline layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,6 @@ export default  {
   content: [
       "./**/*.{js,json,liquid}",
   ],
-  safelist: [
-    'bg-auto',
-    'bg-cover',
-    'bg-contain',
-    'bg-repeat',
-    'bg-no-repeat',
-  ],
   theme: {
     extend: {
       colors: {
